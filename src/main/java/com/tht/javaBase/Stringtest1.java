b56c0f60089670d6a33ff9be87323357498ff5b3
tanght <tanght@11wlw.com>
2018-12-06 17:05:59 +0800

    design pattern complete

---
+++ src/main/java/com/tht/javaBase/Stringtest1.java
@@ -0,0 +1,13 @@
+package com.tht.javaBase;
+
+/**
+ * Created by thrall on 2018/12/5.
+ *
+ */
+public class Stringtest1 {
+    public static void main(String[] args) {
+        //不足四位补足四位   返回String
+        System.out.println(String.format("%04d",12));
+    }
+
+}
